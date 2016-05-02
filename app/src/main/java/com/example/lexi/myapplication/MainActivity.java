416ca4a9ec050b1134dae41dec4a19e1b9b36e4b
Lexi Christiansen <lexi@salesrabbit.com>
2016-05-02 16:28:46 -0600

    Requesting permissions does not work

--- app/src/main/java/com/example/lexi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lexi/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.lexi.myapplication;
 
+import android.Manifest;
 import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
 
         int res = checkCallingOrSelfPermission("android.permission.GET_ACCOUNTS");
         Log.e("Permission: ", (res == PackageManager.PERMISSION_GRANTED) + "");
+        String permissions[] = new String[1];
+        permissions[0] =  Manifest.permission.GET_ACCOUNTS;
+        ActivityCompat.requestPermissions(this, permissions, 0);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        int res = checkCallingOrSelfPermission("android.permission.GET_ACCOUNTS");
+        Log.e("2: ", (res == PackageManager.PERMISSION_GRANTED) + "");
     }
 }
